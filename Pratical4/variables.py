dd16b85bdf6e2351fe54ce7a9ece2f29b649f213
S200606 <shen.24@intl.zju.edu.cn>
2025-03-11 15:36:51 +0800

    2

---
+++ Pratical4/variables.py
@@ -0,0 +1,30 @@
+a = 15
+b = 75
+c = a + b
+d = 90
+e = 5
+f = d + e
+if c > f:
+    print("taking a bus takes more time than driving")
+elif c == f:
+    print("taking a bus takes the same time as driving")
+else:
+    print("taking a bus takes less time than driving")
+
+
+
+X = True
+Y = False
+W = X and Y
+print("W的值是 ", W)
+# W的真值表：
+# X | Y | W (X and Y)
+# ---------------
+# T | T | T
+# T | F | F
+# F | T | F
+# F | F | F
+
+      
+    
+    
