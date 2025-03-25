4139e89aa3fd58f1d5ab4459ce865c98d55a47ed
S200606 <shen.24@intl.zju.edu.cn>
2025-03-25 16:31:59 +0800

    1

---
+++ Practical6/SIR.py
@@ -0,0 +1,42 @@
+#import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+#create the variables
+N = 1000 #the total number of people in the population
+beta = 0.3 #infection probability upon contact beta
+gamma = 0.05 #recovery probability
+I0 = 1 #the initial number of infected people
+R0 = 0 #the initial number of recovered people
+S0 = N - I0 - R0 #the initial number of susceptible people
+
+#create the arrays
+infected = [I0]
+recovered = [R0]
+susceptible = [S0]
+
+#begin cygling
+times = 1000
+for time in range(times):
+    new_R0 = np.random.choice(range(2), I0, p=[1-gamma, gamma]).sum() #the number of recovered people in this round
+    contact_possiblity = beta*I0/N #the probability of contact
+    new_I0 = np.random.choice(range(2), S0, p=[1-contact_possiblity, contact_possiblity]).sum() #the number of infected people in this round
+    new_R0 = min(new_R0, I0) #no larger than I0
+    new_I0 = min(new_I0, S0) #no larger than S0
+    I0 = I0 + new_I0 - new_R0
+    R0 += new_R0
+    S0 -= new_I0  #the number of susceptible people in this round
+    #record the number and append the arrays
+    infected.append(I0)
+    recovered.append(R0)
+    susceptible.append(S0)
+
+#draw the plot
+plt.figure(figsize=(10, 6),dpi=80)  
+plt.plot(infected, label="Infected", color="red")  
+plt.plot(recovered, label="Recovered", color="green")  
+plt.plot(susceptible, label="Susceptible", color="blue")  
+plt.title("SIR Model Simulation")
+plt.xlabel("Time")
+plt.ylabel("Number of People")
+plt.show()
