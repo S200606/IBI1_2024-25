10740db37f18da9ac72fed8dd2235f0aea1873e1
S200606 <shen.24@intl.zju.edu.cn>
2025-04-01 20:15:22 +0800

    2

---
+++ Practical7/spliced_tata_genes.py
@@ -0,0 +1,39 @@
+import re
+donor = input("the donor is:")#input the donor
+acceptor = input("the acceptor is:")#input the acceptor
+input = open(r"c:\Users\jjbcs\Desktop\IBI\IBI_2024-25\IBI1_2024-25\Practical7\tata_genes.fa", "r")#open the file
+# 
+spliced_genes = []#set the list to store the spliced genes
+
+#find all the spliced sequences
+gene_entry = ""#set the gene entry to empty
+for line in input:
+    if line.startswith(">"):
+        if gene_entry:
+            header, sequence = gene_entry.split("\n", 1)
+            genes = re.findall(fr'(?=({donor}.*?{acceptor}))', sequence)
+            genes = [gene for gene in genes if "TATA" in gene]  # select the genes that contain TATA
+            sequence = "".join(genes)  #join the list elements into one line
+            instances_number = len(genes)  #count the number of genes
+            header = f"{header},{instances_number}"
+            spliced_genes.append(f"{header}\n{sequence}")
+        gene_entry = line.strip()
+    else:
+        gene_entry += "\n" + line
+
+#process the last gene entry
+if gene_entry:
+    header, sequence = gene_entry.split("\n", 1)
+    genes = re.findall(fr'(?=({donor}.*?{acceptor}))', sequence)
+    genes = [gene for gene in genes if "TATA" in gene]
+    sequence = "".join(genes)
+    instances_number = len(genes)
+    header = f"{header},{instances_number}"
+    spliced_genes.append(f"{header}\n{sequence}")
+
+#write the result to the file:
+output = open(fr"c:\Users\jjbcs\Desktop\IBI\IBI_2024-25\IBI1_2024-25\Practical7\{donor}{acceptor}_spliced_genes.fa", "w")
+output.write("\n".join(spliced_genes))
+
+
+
