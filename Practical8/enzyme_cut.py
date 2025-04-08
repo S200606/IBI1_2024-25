4df41b189b6cdd8d9007335255707d9a709b0a0a
S200606 <shen.24@intl.zju.edu.cn>
2025-04-08 11:28:16 +0800

    1

---
+++ Practical8/enzyme_cut.py
@@ -0,0 +1,34 @@
+import re
+def check(DNA_sequence, enzyme_cut_site):
+   if re.search("ACGT",DNA_sequence):
+      if re.search("ACGT",enzyme_cut_site):
+         match = re.search(enzyme_cut_site, DNA_sequence)
+         if match:
+          wanted = match.start()+1
+          return wanted
+         else:
+            return "No match"
+            
+      else:
+         return "Enzyme not valid"
+   else:
+       return "DNA sequence not valid"  
+
+# examples
+# Case 1
+dna1 = "ATACGTAGCT"
+enzyme1 = "ACGT"
+print(check(dna1, enzyme1))  # Output: 3
+
+# Case 2
+dna2 = "ACGTXGCT"
+enzyme2 = "AGCT"
+print(check(dna2, enzyme2))  # Output: Enzyme not valid
+
+# Case 3
+dna3 = "AAAAAAA"
+enzyme3 = "ACGT"
+print(check(dna3, enzyme3))  # Output: DNA sequence not valid    
+         
+    
+      
